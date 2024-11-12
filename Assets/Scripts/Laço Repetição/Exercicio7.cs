5fbb65da3903ae97be975f0c54e82d91551af744
Dan-MK9 <danilogith@outlook.com>
2024-11-12 17:12:32 -0300

    Exercicio 7

--- Assets/Scripts/Laço Repetição/Exercicio7.cs
+++ Assets/Scripts/Laço Repetição/Exercicio7.cs
@@ -4,11 +4,24 @@ using static Unity.Burst.Intrinsics.X86;
 
 public class Exercicio7 : MonoBehaviour
 {
-//    7.(Maior número em uma lista) Dada uma lista de números,
-//      encontre o maior valor.
+    //    7.(Maior número em uma lista) Dada uma lista de números,
+    //      encontre o maior valor.
+
+    int[] numeros = { -3, -5, -2, -12, -21, -96, -87, -7, -9, -10 };
+    int maior = 0;
     void Start()
     {
-        
+        maior = numeros[0];
+
+        foreach (var item in numeros)
+        {
+            if (item > maior)
+            {
+                maior = item;
+            }
+
+            print("O maior numero é " + maior);
+        }
     }
 
     void Update()
